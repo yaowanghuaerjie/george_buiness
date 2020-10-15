baa62de2a4e4d3fcbbca72e05ed8a25990943969
wulong <wulong@carsir.xin>
2020-10-15 21:13:00 +0800

    初始化项目

---
+++ lib_image_loader/src/main/java/com/george/lib_image_loader/app/ImageLoaderManager.java
@@ -0,0 +1,184 @@
+package com.george.lib_image_loader.app;
+
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RemoteViews;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.Priority;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
+import com.bumptech.glide.request.target.NotificationTarget;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.target.Target;
+import com.bumptech.glide.request.transition.Transition;
+import com.george.lib_image_loader.R;
+import com.george.lib_image_loader.image.CustomRequestListener;
+import com.george.lib_image_loader.image.Utils;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
+
+import static com.bumptech.glide.load.resource.bitmap.BitmapTransitionOptions.withCrossFade;
+
+public class ImageLoaderManager {
+    private ImageLoaderManager() {
+    }
+
+    public ImageLoaderManager getInstance() {
+        return SingletonHolder.instance;
+    }
+
+    private static class SingletonHolder {
+        private static ImageLoaderManager instance = new ImageLoaderManager();
+    }
+
+    /**
+     * 为notification加载图
+     *
+     * @param context         上下文
+     * @param rv              RemoteView
+     * @param id              viewId
+     * @param notification
+     * @param NOTIFICATION_ID 布局id
+     * @param url             图片URL
+     */
+    public void dispalyImageForNotification(Context context, RemoteViews rv, int id, Notification notification, int NOTIFICATION_ID, String url) {
+        this.displayImageForTarget(context, initNotificationTarget(context, id, rv, notification, NOTIFICATION_ID), url);
+    }
+
+    /**
+     * 不带回调的加载
+     *
+     * @param imageView
+     * @param url
+     */
+    public void displayImageForView(ImageView imageView, String url) {
+        this.displayImageForView(imageView, url, null);
+    }
+
+    /**
+     * 带回调的加载图片方法
+     *
+     * @param imageView
+     * @param url
+     * @param requestListener
+     */
+    public void displayImageForView(ImageView imageView, String url,
+                                    CustomRequestListener requestListener) {
+        Glide.with(imageView.getContext())
+                .asBitmap()
+                .load(url)
+                .apply(initCommonRequestOption())
+                .transition(withCrossFade())
+                .into(imageView);
+    }
+
+    /**
+     * 加载圆形图片
+     *
+     * @param imageView
+     * @param url
+     */
+    public void displayImageForCircle(final ImageView imageView, String url) {
+        Glide.with(imageView.getContext())
+                .asBitmap()
+                .load(url)
+                .apply(initCommonRequestOption())
+                .into(new BitmapImageViewTarget(imageView) {
+                    @Override
+                    protected void setResource(final Bitmap resource) {
+                        RoundedBitmapDrawable circularBitmapDrawable =
+                                RoundedBitmapDrawableFactory.create(imageView.getResources(), resource);
+                        circularBitmapDrawable.setCircular(true);
+                        imageView.setImageDrawable(circularBitmapDrawable);
+                    }
+                });
+    }
+
+    public void displayImageForViewGroup(final ViewGroup group, String url) {
+        Glide.with(group.getContext())
+                .asBitmap()
+                .load(url)
+                .apply(initCommonRequestOption())
+                .into(new SimpleTarget<Bitmap>() {
+                    @SuppressLint("CheckResult")
+                    @Override
+                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                        final Bitmap res = resource;
+                        Observable.just(resource)
+                                .map(new Function<Bitmap, Drawable>() {
+                                    @Override
+                                    public Drawable apply(Bitmap bitmap) throws Exception {
+                                        Drawable drawable = new BitmapDrawable(
+                                                Utils.doBlur(res, 100, true)
+                                        );
+                                        return drawable;
+                                    }
+                                })
+                                .subscribeOn(Schedulers.io())
+                                .observeOn(AndroidSchedulers.mainThread())
+                                .subscribe(new Consumer<Drawable>() {
+                                    @Override
+                                    public void accept(Drawable drawable) throws Exception {
+                                        group.setBackground(drawable);
+                                    }
+                                });
+                    }
+                });
+    }
+
+    private void displayImageForTarget(Context context, Target target, String url) {
+        Glide.with(context)
+                .asBitmap()
+                .load(url)
+                .apply(initCommonRequestOption())
+                .transition(withCrossFade())
+                .fitCenter()
+                .into(target);
+    }
+
+    private void displayImageForTarget(Context context, Target target, String url,
+                                       CustomRequestListener requestListener) {
+        Glide.with(context)
+                .asBitmap()
+                .load(url)
+                .apply(initCommonRequestOption())
+                .transition(withCrossFade())
+                .fitCenter()
+                .listener(requestListener)
+                .into(target);
+    }
+
+    private NotificationTarget initNotificationTarget(Context context, int id, RemoteViews rv,
+                                                      Notification notification, int NOTIFICATION_ID) {
+        return new NotificationTarget(context, id, rv, notification, NOTIFICATION_ID);
+    }
+
+    @SuppressLint("CheckResult")
+    private RequestOptions initCommonRequestOption() {
+        RequestOptions options = new RequestOptions();
+        options.placeholder(R.mipmap.b4y)
+                .error(R.mipmap.b4y)
+                .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+                .skipMemoryCache(false)
+                .priority(Priority.NORMAL);
+        return options;
+    }
+
+
+}
---
+++ lib_image_loader/src/main/java/com/george/lib_image_loader/image/CustomRequestListener.java
@@ -0,0 +1,6 @@
+package com.george.lib_image_loader.image;
+
+import com.bumptech.glide.request.RequestListener;
+
+public interface CustomRequestListener extends RequestListener {
+}
