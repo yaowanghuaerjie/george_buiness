1ccf1b78bbf10f3d01f1e3e074d7b6a4e57cea11
wulong <wulong@carsir.xin>
2020-11-04 22:20:00 +0800

    初始化CommonUI库

--- lib_common_ui/src/main/java/com/george/lib_common_ui/base/BaseActivity.java
+++ lib_common_ui/src/main/java/com/george/lib_common_ui/base/BaseActivity.java
@@ -1,4 +1,55 @@
 package com.george.lib_common_ui.base;
 
-public class BaseActivity {
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+
+import com.george.lib_common_ui.base.constant.Constant;
+
+public class BaseActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public void requestPermission(int code, String... permissions) {
+
+    }
+
+    public boolean hasPermission(String... permissions) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case Constant
+                    .WRITE_READ_EXTERNAL_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    doSDCardPermission();
+                }
+                break;
+            case Constant.HARDWEAR_CAMERA_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    doCameraPermission();
+                }
+                break;
+        }
+    }
+
+    private void doSDCardPermission() {
+
+    }
+
+    private void doCameraPermission() {
+
+    }
 }
--- lib_common_ui/src/main/java/com/george/lib_common_ui/base/constant/Constant.java
+++ lib_common_ui/src/main/java/com/george/lib_common_ui/base/constant/Constant.java
@@ -1,4 +1,16 @@
 package com.george.lib_common_ui.base.constant;
 
+
+import android.Manifest;
+
 public class Constant {
+    public static final int WRITE_READ_EXTERNAL_CODE = 0x01;
+    public static final String[] WRITE_READ_EXTERNAL_PERMISSION = new String[]{
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
+    public static final int HARDWEAR_CAMERA_CODE = 0x02;
+    public static final String[] HARDWEAR_CAMERA_PERMISSION = new String[]{
+            Manifest.permission.CAMERA
+    };
 }
